96571de8a0255ecb80ae363e54bfe428394af93d
tkdgns1313 <tkdgns757@naver.com>
2021-04-26 05:49:23 +0900

    중간고사 대비

--- game.py
+++ game.py
@@ -26,16 +26,25 @@ print("업비트 접속 완료.")
 print("얼마를 충전하시겠습니까? ")
 money = int(input("충전금액 : "))
 print(money,"원이 충전되었습니다.\n")
+doge = int(money*1.4)
+bitcoin = int(money*0.7)
 
-
-print("1.패닉셀한다. 2.추가매수한다. 3.한숨쉬고 업비트를 끈다. ")
+print("충전된 돈",money,"원으로 어떤코인을 구매할까요?")
+print("1.도지코인. 2.비트코인 3.수면매수 ")
 num = int(input("선택 : "))
 
 if(num==1):
-    print("패닉셀을 하자, 다시 반등이 왔습니다 ㅋㅋ.")
+    print("일론머스크의 트윗질로 가격이 올라갑니다")
+    print("현재 잔고 : ",doge)
 if(num==2):
-    print("추가매수하자, 다시 떨어집니다 ㅋㅋ.")
+    print("비트코인의 하락장에 갇혔습니다.")
+    print("현재 잔고 : ",bitcoin)
 if(num==3):
-    num1 = int(input("예상 손실 : "))
-    num2 = int(input("예상 손실 : "))
-    print("업비트에서",sonsil(num1, num2),"%의 손실알림이 도착했습니다. 어떻게 하시겠습니까?")3
+    num1 = int(input("예상 이득 퍼센트 : "))
+    num2 = int(input("예상 손해 퍼센트 : "))
+    print("업비트에서",sonsil(num1,num2),"%의 손실알림이 도착했습니다.")
+    
+    num3 = int(money * ((1.0 - (num1+num2)) / 100.0))
+
+    print("현재 잔고 :", num3)
+
